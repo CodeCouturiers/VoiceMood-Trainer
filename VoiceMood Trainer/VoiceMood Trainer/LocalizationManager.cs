1b98d9a209841f2c478a6bcf71170b0e0e697363
x_ConnectionIssueDetected_x <x_ConnectionIssueDetected_x@users.noreply.github.com>
2024-08-02 01:46:30 +0300

    nvm

--- VoiceMood Trainer/VoiceMood Trainer/LocalizationManager.cs
+++ VoiceMood Trainer/VoiceMood Trainer/LocalizationManager.cs
@@ -1,14 +1,6 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 using System.IO;
 using System.Windows;
-using Newtonsoft.Json;
 namespace VoiceMood_Trainer
 {
 
